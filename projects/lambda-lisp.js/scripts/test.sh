4a1ff0a180f04d15a79f569414c8e0c235c67d1b
Xie Yuheng <xyheme@gmail.com>
2026-03-18 02:31:35 +0800

    up

--- projects/lambda-lisp.js/scripts/test.sh
+++ projects/lambda-lisp.js/scripts/test.sh
@@ -6,6 +6,6 @@ parallel="parallel -v --halt now,fail=1"
 bin="node ./lib/main.js run"
 flags=""
 
-find lisp/tests -name "*.test.lisp" | $parallel $bin {} $flags
-find lisp/tests -name "*.snapshot.lisp" | $parallel $bin {} $flags ">" {}.out
-find lisp/tests -name "*.error.lisp" | $parallel $bin {} $flags ">" {}.err "||" true
+find lisp -name "*.test.lisp" | $parallel $bin {} $flags
+find lisp -name "*.snapshot.lisp" | $parallel $bin {} $flags ">" {}.out
+find lisp -name "*.error.lisp" | $parallel $bin {} $flags ">" {}.err "||" true
