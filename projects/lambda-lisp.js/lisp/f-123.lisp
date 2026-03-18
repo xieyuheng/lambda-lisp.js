2a95ca8bd2bb5cd0576a3d095f8a234c3896b4c3
Xie Yuheng <xyheme@gmail.com>
2026-03-18 14:54:32 +0800

    up

--- projects/lambda-lisp.js/lisp/f-123.lisp
+++ projects/lambda-lisp.js/lisp/f-123.lisp
@@ -1,14 +1,20 @@
 (define (f1 n) (f1 n))
 (define (g1 n) (g1 n))
+
 (assert-bisimilar f1 g1)
 
 (define (f2 n) (f2 (f2 n)))
 (define (g2 n) (g2 (g2 n)))
+
 (assert-bisimilar f2 g2)
+;; TODO fail
+;; (assert-bisimilar f2 (lambda (n) (f2 (f2 (f2 n)))))
+;; (assert-bisimilar (lambda (n) (f2 (f2 n))) (lambda (n) (f2 (f2 (f2 n)))))
 
 (define (f3 n) (f3 (f3 (f3 n))))
 (define (g3 n) (g3 (g3 (g3 n))))
+
 (assert-bisimilar f3 g3)
 
-;; TODO
+;; TODO fail
 ;; (assert-bisimilar f2 f3)
